0d5ecd99a52a68f7e28483ec40f9c1ab684d8e90
Ray Hunter <ray.hunter@gmail.com>
2020-08-12 20:39:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+
+function CustomInput({ field, form, ...props }) {
+  return <input {...field} {...props} />
+}
 
 function App() {
+  const [result, setResult] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Formik is the main component that handles all the logic
+    // Form is just a regular html <form> wrapper
+    <Formik
+      initialValues={{
+        email: '',
+        name: '',
+        color: 'red'
+      }}
+      onSubmit={(values, actions) => {
+        setResult(JSON.stringify(values))
+      }}
+    >
+      {() => (
+        <Form>
+          <Field as="select" name="color" value="none">
+            <option value="none">Pick a color</option>
+            <option value="red">Red</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+          </Field>
+          <br />
+          <Field name="email">
+            {({ field }) => (
+              <div>
+                <label>email:</label>
+                <input
+                  type="email"
+                  required
+                  placeholder="Email"
+                  {...field}
+                />
+              </div>
+            )}
+          </Field>
+          <br />
+          <Field
+            name="name"
+            required
+            placeholder="Name"
+            component={CustomInput}
+          />
+          <br />
+          <button type="submit">Submit</button>
+          <br />
+          <br />
+          <Field
+            style={{ width: '100%'}}
+            as="textarea"
+            value={result}
+            rows={2}
+          />
+        </Form>
+      )}
+    </Formik>  
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { render, fireEvent, waitFor, waitForDomChange } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
+
+it("submits correct values", async () => {
+  const { container } = render(<App />)
+  const name = container.querySelector('input[name="name"]')
+  const email = container.querySelector('input[name="email"]')
+  const color = container.querySelector('select[name="color"]')
+  const submit = container.querySelector('button[type="submit"]')
+  const results = container.querySelector("textarea");
+
+  await waitFor(() => {
+    fireEvent.change(name, {
+      target: {
+        value: "mockname"
+      }
+    })
+  })
+
+  await waitFor(() => {
+    fireEvent.change(email, {
+      target: {
+        value: "mock@email.com"
+      }
+    })
+  })
+
+  await waitFor(() => {
+    fireEvent.change(color, {
+      target: {
+        value: "green"
+      }
+    })
+  })
+
+  await waitFor(() => {
+    fireEvent.click(submit)
+  })
+
+  expect(results.innerHTML).toBe(
+    '{"email":"mock@email.com","name":"mockname","color":"green"}'
+  )
+})
